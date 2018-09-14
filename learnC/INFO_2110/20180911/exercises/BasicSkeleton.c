9426c697ac9392563bddc982a57d1045e5ecd089
ClariNerd617 <HGoodwin617@gmail.com>
2018-09-14 17:45:47 -0400

    first day

---
+++ learnC/INFO_2110/20180911/exercises/BasicSkeleton.c
@@ -0,0 +1,31 @@
+/***************************************************************************
+ *  Name:      
+ *  Date:
+ *  Project Description:
+ *
+ ***************************************************************************/
+ 
+ //Libraries
+ #include "stdafx.h"	//C++ Library
+ #include <stdio.h>		//C Library
+ 
+ //Functions
+ 
+
+
+/**************************************************************************/
+
+int main(void)
+{
+	//execution begins in the main()
+
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;	//indicates program completion
+}
---
+++ learnC/INFO_2110/20180911/exercises/ex1.c
@@ -0,0 +1,31 @@
+/***************************************************************************
+ *  Name: Henry Goodwin      
+ *  Date: 20180911
+ *  Project Description: First in class exercises
+ *
+ ***************************************************************************/
+ 
+//Libraries
+//#include "stdafx.h"   //C++ Library
+#include <stdio.h>      //C Library
+ 
+//Functions
+ 
+
+
+/**************************************************************************/
+
+int main(void)
+{
+    //execution begins in the main()
+
+    int sum; //declare integer variable sum
+
+    sum=50+25; //add 50 and 25 and assign the result to sum
+
+    //print a sentence with the answer
+    printf("The sum of 50 and 25 is %i\n",sum);
+    //%i is a placeholder for sum
+
+    return 0;
+}
