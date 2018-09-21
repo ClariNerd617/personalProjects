405333dfe125b444f26217a0a70a26a7b4fa0a69
ClariNerd617 <HGoodwin617@gmail.com>
2018-09-21 16:56:09 -0400

    Finished the homework

--- learnC/INFO_2110/20180918/homework/homework1.c
+++ learnC/INFO_2110/20180918/homework/homework1.c
@@ -30,52 +30,55 @@ Part D: (95 pts)
 ********************************************************************************************/
 
 	
-//libraries
-#include <stdio.h>	//C
-
+// Libraries
+#include <stdio.h>    //C
+// Global Variables
+int a,b,c,r;
+float pi = 3.14159;
 // Using the gcc 7.3.0 compiler
+
 
 void PartA(void)
 {
-	/*	Write a program that asks the user to enter two whole numbers, 
-	and prints the sum, product, difference, quotient and remainder 
-	of the two numbers.   */
+    /* Write a program that asks the user to enter two whole numbers, and prints the sum, product, difference, quotient and remainder of the two numbers. */
 
-	//Declare the variables
-	int a,b,s,d,p,q,m;
-
-	// Prompt the user for variables a and b
-	printf("Enter an integer: ");
-	scanf("%i",&a);
-	printf("Enter another interger: ");
-	scanf("%i",&b);
+    //Declare the variables
+    int s,d,p,q,m;
 	
-	// Calculations
+    //Prompt User for Input
 
-	s = a + b; //s is sum
-	d = a - b; //d is difference
-	p = a * b; //p is product
-	q = a / b; //q is quotient
-	m = a % b; //m is modulus
+    printf("Enter an integer: ");
+    scanf("%i",&a);
+    printf("Enter another integer: ");
+    scanf("%i",&b);
 
-	// Display the results
-	printf("The Sum of %i and %i is: %i.\n",a,b,s);
-	printf("The Difference between %i and %i is: %i.\n",a,b,d);
-	printf("The Product of %i and %i is: %i.\n",a,b,p);
-	printf("The Quotient of %i and %i is: %i.\n",a,b,q);
-	printf("The Modulus of %i and %i is: %i.\n\n",a,b,m);
+    // Calculations
+
+    s = a + b; //s is sum
+    d = a - b; //d is difference
+    p = a * b; //p is product
+    q = a / b; //q is quotient
+    m = a % b; //m is modulus
+
+    // Display the results
+    printf("The Sum of %i and %i is: %i.\n",a,b,s);
+    printf("The Difference between %i and %i is: %i.\n",a,b,d);
+    printf("The Product of %i and %i is: %i.\n",a,b,p);
+    printf("The Quotient of %i and %i is: %i.\n",a,b,q);
+    printf("The Modulus of %i and %i is: %i.\n\n",a,b,m);
+
 }
 
 void PartB(void)
 {
-	/* Part A and write a program that gets three different whole numbers 
-	from the user, then prints the sum, the average, the product of 
-	these numbers.   */
+    /* Part A and write a program that gets three different whole numbers 
+    from the user, then prints the sum, the average, the product of 
+    these numbers.*/
 	
     // Declare Variables (m is mean)
-    int a,b,c,s,m,p;
-
-    // Prompt the user for a b and c
+    int s,m,p;
+    
+    // Prompt user for input
     printf("Enter an integer: ");
     scanf("%i",&a);
     printf("Enter another integer: ");
@@ -93,37 +96,59 @@ void PartB(void)
     printf("The Sum of %i, %i, and %i is %i.\n",a,b,c,s);
     printf("The Mean of %i, %i, and %i is %i.\n",a,b,c,m);
     printf("The Product of %i, %i, and %i is %i.\n\n",a,b,c,p);
+
+
 }
 
 void PartC(void)
 {
-	/*    Part A & B and write a program that reads in the radius of a circle 
-	and prints the circle's diameter, circumference and area.  Use the 
-	constant value 3.14159 for Pi.  All calculations should occur outside 
-	the printf statement(s) and use the formatted conversion %.2f for float variables.  */
-	
-	
-	
+    /* Part A & B and write a program that reads in the radius of a circle and prints the circle's diameter, circumference and area.  Use the constant value 3.14159 for Pi. All calculations should occur outside the printf statement(s) and use the formatted conversion %.2f for float variables. */
+    
+    // Prompt the user for the radius of the circle.
+    float circ,area;
+    int d;
+    printf("Enter the radius of the circle: ");
+    scanf("%i",&r);
+
+    // Calculations and Display the Results
+   
+    
+    d = 2*r;
+    printf("The diameter of the circle is %i.\n",d);
+
+    circ = 2 * pi * r;
+    printf("The circumference of the circle is %f.\n",circ);
+
+    area = pi * (r * r);
+    printf("The Area of the circle is %f.\n",area);
 	
 }
 
 void PartD(void)
 {
-	/*	Part A, B, & C and write a program that calculates the student average based on 
-	the following where the user enters 3 values, the program adds the values up and 
-	then displays the student average  dividing the sum by 10.   */
-	
-	
-	
-	
+    /* Part A, B, & C and write a program that calculates the student average based on the following where the user enters 3 values, the program adds the values up and then displays the student average  dividing the sum by 10.  */
+
+    // Get the values for the variables
+    
+    int A,B,C,G;
+    printf("Enter the grades here: ");
+    scanf("%i",&A); scanf("%i",&B); scanf("%i",&C);
+    
+    // Calculations
+    
+    G = (A + B + C) / 10;
+
+    // Display the results
+    
+    printf("The Student Average is: %i.\n",G);
 }
 
 int main(void)
 {
-	PartA();
-	PartB();
-	PartC();
-	PartD();
+    PartA();
+    PartB();
+    PartC();
+    PartD();
 	
-	return 0;
+    return 0;
 }
