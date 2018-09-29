86cba13a0bb744c9b97bb365918b431cd6c28b7c
ClariNerd617 <HGoodwin617@gmail.com>
2018-09-29 17:15:01 -0400

    end of lesson

--- learnC/INFO_2110/20180925/exercises/ex3.c
+++ learnC/INFO_2110/20180925/exercises/ex3.c
@@ -16,7 +16,7 @@ int main(void)
     printf("          MENU\n");
     printf("    1      -       Addition\n");
     printf("    2      -       Subtraction\n");
-    printf("    3      -       Multiplicatoin\n");
+    printf("    3      -       Multiplication\n");
     printf("    4      -       Division\n");
     printf("    5      -       Quit\n");
 
