2091342ed44e93ec514f2843e83c5efc2ee2d4f1
Rolf Henry Goodwin <HGoodwin617@gmail.com>
2019-01-09 15:08:15 -0500

    Switch

---
+++ learnJava/Switch/src/com/clarinerd/Main.java
@@ -0,0 +1,36 @@
+package com.clarinerd;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*        int value = 3;
+        if (value == 1) {
+            System.out.println("Value was 1");
+        } else if (value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }*/
+
+        int switchValue = 4;
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3: case 4: case 5:
+                System.out.println("was a 3, or a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+
+            default:
+                System.out.println("Was not 1, 2, 3, 5, or 5");
+                break;
+        }
+    }
+}
