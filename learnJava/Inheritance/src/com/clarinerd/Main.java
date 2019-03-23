4de328dbd66f37d4f38f6ec9d0008ed726a24b36
ClariNerd617 <HGoodwin617@gmail.com>
2019-03-22 23:55:55 -0400

    m

--- learnJava/Inheritance/src/com/clarinerd/Main.java
+++ learnJava/Inheritance/src/com/clarinerd/Main.java
@@ -3,7 +3,7 @@ package com.clarinerd;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+// write your code here
         System.out.print("hello");
     }
 }
