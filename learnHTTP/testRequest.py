3093b2823c45e4e320d35129014cf12382b4b570
ClariNerd617 <HGoodwin617@gmail.com>
2019-05-22 12:04:30 -0400

    merge

--- learnHTTP/testRequest.py
+++ learnHTTP/testRequest.py
@@ -1,33 +1,41 @@
 import requests as req
 
-urls = ['https://jsonplaceholder.typicode.com/posts/1',
-        'https://jsonplaceholder.typicode.com/posts']
+class health:
 
-dummy_data = {'id':1,
-            'title':'eggs',
-            'body':'spam',
-            'userId':1}
+    def check():
+        if response.status_code in [200, 201]:
+            message = 'success'
+        elif response.status_code is 204:
+            if response.headers is not None:
+                options = response.headers['Access-Control-Allow-Methods']
+                message = f'{options}'
+            else:
+                message = 'error: no options'
+        elif response.status_code is 404:
+            message = 'not found'
+        else:
+            message = 'error: invalid status code'
+        return f'{response.status_code}: {message}'
+    
+    urls = ['https://jsonplaceholder.typicode.com/posts/1',
+            'https://jsonplaceholder.typicode.com/posts']
+        
+    dummy_data = {'id': 1,
+                'title': 'eggs',
+                'body': 'spam',
+                'userId': 1}
 
-def health_check():
-    if response.status_code is 200 or 201 or 204:
-        message = f'{response.status_code}: success'
-    elif response.status_code is 404:
-        message = f'{response.status_code}: not found'
-    else:
-        message = 'error: invalid status code'
-    return message
+response = req.get(health.urls[0])
+print(health.check())
 
-response = req.get(urls[0])
-print(health_check())
+response = req.post(health.urls[1], health.dummy_data)
+print(health.check())
 
-response = req.post(urls[1],dummy_data)
-print(health_check())
+response = req.put(health.urls[0], health.dummy_data)
+print(health.check())
 
-response =req.put(urls[0],dummy_data)
-print(health_check())
+response = req.delete(health.urls[0])
+print(health.check())
 
-response = req.delete(urls[0])
-print(health_check())
-
-response = req.options(urls[0])
-print(health_check())
+response = req.options(health.urls[0])
+print(health.check())
