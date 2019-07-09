d75b5724fa4f14010e41fd6cbc12ad94782513f9
Rolf Henry Goodwin <ClariNerd617@users.noreply.github.com>
2019-07-09 16:44:23 -0400

    Plans for the future
    Will work for more than just the office

--- weather/initial_conditions.py
+++ weather/initial_conditions.py
@@ -1,5 +1,16 @@
-import requests, json
+import requests
+from json import dumps
 
+# def get_loc():
+# TODO: Implement this, should return [lat, lon]
+# using Google's geocode REST API
+# https://developers.google.com/maps/documentation/geocoding/start
+# lat = loc['geometry']['location']['lat']
+# lon = loc['geometry']['location']['lon']
+# response = requests.get(f'https://api.weather.gov/points/{get_loc[0]},{get_loc[1]}').json()
+
+# forecast_url = response['properties']['forecast']
+# response = requests.get(forecast_url).json()
 response = requests.get('https://api.weather.gov/gridpoints/GYX/41,12').json()
 
 
@@ -18,7 +29,7 @@ initial_values = []
 for key in keys:
   initial_values.append(get_value(key))
 
-initial_conditions = json.dumps(dict(zip(keys, initial_values)), indent=1)
+initial_conditions = dumps(dict(zip(keys, initial_values)), indent=1)
 
 print(initial_conditions)
 
