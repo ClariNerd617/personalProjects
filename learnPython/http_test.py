70ad45ba38ac41f582cf2563d11cf471ff59c79c
Rolf Henry Goodwin <ClariNerd617@users.noreply.github.com>
2019-07-09 16:12:54 -0400

    commits

--- learnPython/http_test.py
+++ learnPython/http_test.py
@@ -1,18 +1,48 @@
-import requests
+import requests as req
 
-url = 'https://jsonplaceholder.typicode.com/todos/1'
-response = requests.get(url)
-
-
-def check_request():
-	if response.status_code is 200:
-		message = 'success'
-	elif response.status_code is 404:
-		message = 'not found'
+def check():
+	if r.status_code in [200, 201]:
+		m = 'success'
+	elif r.status_code is 204:
+		if r.headers is not None:
+			opts = r.headers['Access-Control-Allow-Methods']
+			m = opts
+		else:
+			m = 'error: no options'
+	elif r.status_code is 404:
+		m = 'not found'
 	else:
-		message = 'error'
-	return message
+		m = 'error: invalid status code'
+	return f'{r.status_code}: {m}'
 
 
-print(check_request())
+urls = [
+	'https://jsonplaceholder.typicode.com/posts/1',
+	'https://jsonplaceholder.typicode.com/posts'
+]
 
+data = {'id': 1, 'title': 'eggs', 'body': 'spam', 'userId': 1}
+
+# HTTP GET
+for i in range(2):
+	r = req.get(urls[i])
+	print(f'GET TEST: {check()}\n')
+
+# HTTP POST
+r = req.post(urls[1], data)
+print(f'POST TEST: {check()}\n')
+
+# HTTP PUT
+r = req.put(urls[0], data)
+print(f'PUT TEST: {check()}\n')
+
+# HTTP DELETE
+r = req.delete(urls[0])
+print(f'DELETE TEST: {check()}\n')
+
+# HTTP OPTIONS
+for i in range(2):
+	r = req.options(urls[i])
+	print(f'OPTIONS TEST: {check()}\n')
+
+print('yaaaas')
