e2b9f3323e2a218b150ad32f913d731f747ad51b
ClariNerd617 <HGoodwin617@gmail.com>
2019-07-09 20:20:12 -0400

    stuff

--- weather/initial_conditions.py
+++ weather/initial_conditions.py
@@ -15,21 +15,20 @@ response = requests.get('https://api.weather.gov/gridpoints/GYX/41,12').json()
 
 
 def get_value(var):
-  val = round(response['properties'][f'{var}']['values'][0]['value'], 2)
-  return val
+    val = round(response['properties'][f'{var}']['values'][0]['value'], 2)
+    return val
 
 
 keys = [
-  'temperature', 'dewpoint', 'maxTemperature', 'minTemperature',
-  'relativeHumidity', 'apparentTemperature', 'heatIndex', 'windChill',
-  'windDirection', 'windSpeed', 'windGust', 'probabilityOfPrecipitation'
+    'temperature', 'dewpoint', 'maxTemperature', 'minTemperature',
+    'relativeHumidity', 'apparentTemperature', 'heatIndex', 'windChill',
+    'windDirection', 'windSpeed', 'windGust', 'probabilityOfPrecipitation'
 ]
 
 initial_values = []
 for key in keys:
-  initial_values.append(get_value(key))
+    initial_values.append(get_value(key))
 
 initial_conditions = dumps(dict(zip(keys, initial_values)), indent=1)
 
 print(initial_conditions)
-
